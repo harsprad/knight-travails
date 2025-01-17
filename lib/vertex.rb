9d22723ee6161f3105094a313475671b82dd42c7
harsprad <101750118+harsprad@users.noreply.github.com>
2025-01-17 15:16:52 +0000

    Feat: Vertex class

--- lib/vertex.rb
+++ lib/vertex.rb
@@ -1,2 +1,16 @@
 class Vertex
+  attr_accessor :value, :adjacent
+
+  def initialize(coord)
+    @value = coord
+    @adjacent = []
+  end
+
+  def self.connect(vertex_1, vertex_2)
+    return if vertex_1.nil? || vertex_2.nil?
+    return if vertex_1.value == vertex_2.value
+
+    vertex_1.adjacent << vertex_2
+    vertex_2.adjacent << vertex_1
+  end
 end
